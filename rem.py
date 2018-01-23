fe2fb6371a998d65ee4df2331e2e89a055da5684
matyushkin <leva.matyushkin@gmail.com>
2018-01-23 12:49:40 +0300

    Improve quality of xls file

--- rem.py
+++ rem.py
@@ -8,6 +8,21 @@ from xlwt import Workbook
 excel_file = xlwt.Workbook()
 sheet = excel_file.add_sheet('Memories')
 
+memory_col = sheet.col(2)
+memory_col.width = 256*72   # 72 symbols width
+
+alignment = xlwt.Alignment()
+alignment.wrap = 1
+alignment.horz = xlwt.Alignment.HORZ_LEFT # May be: HORZ_GENERAL, HORZ_LEFT, HORZ_CENTER, HORZ_RIGHT, HORZ_FILLED, HORZ_JUSTIFIED, HORZ_CENTER_ACROSS_SEL, HORZ_DISTRIBUTED
+alignment.vert = xlwt.Alignment.VERT_TOP # May be: VERT_TOP, VERT_CENTER, VERT_BOTTOM, VERT_JUSTIFIED, VERT_DISTRIBUTED
+
+style = xlwt.XFStyle()
+style.alignment = alignment
+
+sheet.write(0, 0, '№')
+sheet.write(0, 1, 'Имя')
+sheet.write(0, 2, 'Воспоминание')
+
 codes = re.findall('\[(\d*)\]', text)
 names  = re.findall('\][\s\t]([^:.]*):?\s?\n', text)
 texts = []
@@ -26,8 +41,8 @@ for i in range(len(codes)):
 
 for code, name, t in zip(codes, names, texts):
     code = int(code)
-    sheet.write(code, 0, code)
-    sheet.write(code, 1, name)
-    sheet.write(code, 2, t)
+    sheet.write(code, 0, code, style)
+    sheet.write(code, 1, name, style)
+    sheet.write(code, 2, t, style)
 
 excel_file.save("Data.xls")    
